95cc7d6cda4a86c034848bebd5b7614698624423
R-Szk <trail_stuff_seat@yahoo.co.jp>
2024-04-20 23:27:03 +0900

    プログレスバーのアニメーションさせる用のViewModelを追加し、それがViewに反映されるように実装

--- MyTimerApp/ContentView.swift
+++ MyTimerApp/ContentView.swift
@@ -5,9 +5,13 @@
 //  Created by 鈴木龍斗 on 2024/04/20.
 //
 
+import Foundation
 import SwiftUI
+import Combine
 
 struct ContentView: View {
+    // ViewModelをStateObjectで宣言してCircleProgressViewModelを監視でkるようにする
+    @StateObject var viewModel = CircleProgressViewModel()
     
     var body: some View {
         ZStack {
@@ -20,13 +24,42 @@ struct ContentView: View {
                 .frame(width: 300, height: 300)
             // 上の円
             Circle()
-                .trim(from: 0.0, to: 0.5)
+                .trim(from: 0.0, to: viewModel.progressValue)
                 .stroke(
                     Color.orange,
                     style: StrokeStyle(lineWidth: 15, lineCap: .round))
                 .frame(width: 300, height: 300)
+                .rotationEffect(Angle(degrees: -90))
         }
         .padding()
+    }
+}
+
+final class CircleProgressViewModel: ObservableObject {
+    @Published var progressValue: CGFloat = 0.0
+    private var timerCount: CGFloat = 0.0
+    private var cancellabel: AnyCancellable?
+    
+    init() {
+        startTimer()
+    }
+    
+    private func startTimer() {
+        timerCount = 60
+        
+        cancellabel = Timer.publish(every: 0.1, on: .main, in: .default)
+            .autoconnect()
+            .sink { [weak self] _ in
+                self?.countProgress()
+            }
+    }
+    
+    private func countProgress() {
+        if timerCount < 0 {
+            cancellabel?.cancel()
+        }
+        timerCount = timerCount - 0.1
+        progressValue = timerCount / 60
     }
 }
 
