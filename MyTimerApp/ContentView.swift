e7d8fe666f1cbbf72134339d9456eac1a6089806
R-Szk <trail_stuff_seat@yahoo.co.jp>
2024-04-20 23:06:03 +0900

    タイマーのプログレスバーのモックを作成

--- MyTimerApp/ContentView.swift
+++ MyTimerApp/ContentView.swift
@@ -8,12 +8,23 @@
 import SwiftUI
 
 struct ContentView: View {
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        ZStack {
+            // 下の円
+            Circle()
+                .stroke(
+                    Color.gray,
+                    style: StrokeStyle(lineWidth: 15, lineCap: .round))
+                .opacity(0.5) // 透明度
+                .frame(width: 300, height: 300)
+            // 上の円
+            Circle()
+                .trim(from: 0.0, to: 0.5)
+                .stroke(
+                    Color.orange,
+                    style: StrokeStyle(lineWidth: 15, lineCap: .round))
+                .frame(width: 300, height: 300)
         }
         .padding()
     }
