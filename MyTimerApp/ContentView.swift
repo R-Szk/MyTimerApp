5d48b75ee763cd3f0a3d77b7b67570db2c7451c2
R-Szk <trail_stuff_seat@yahoo.co.jp>
2024-04-20 22:52:32 +0900

    Initial Commit

---
+++ MyTimerApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MyTimerApp
+//
+//  Created by 鈴木龍斗 on 2024/04/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MyTimerApp/MyTimerAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyTimerAppApp.swift
+//  MyTimerApp
+//
+//  Created by 鈴木龍斗 on 2024/04/20.
+//
+
+import SwiftUI
+
+@main
+struct MyTimerAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
